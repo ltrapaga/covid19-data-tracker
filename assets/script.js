c84b01cad9370824798b6654ddaf151e03f977d9
Laura Trapaga <trapaga9@gmail.com>
2022-12-09 13:52:15 -0500

    Attempt Google Charts API.

--- assets/script.js
+++ assets/script.js
@@ -23,7 +23,13 @@ function fetchCovidData(cityAC) {
             console.log("Population: " + totPopulation);
             // Grab total vaccinations administered
             let totVaccinesAdministered = res.actuals.vaccinesAdministered;
-            console.log("Total Vaccinations Administered: " + totVaccinesAdministered);
+            let totVaccinesCompleted = res.actuals.vaccinationsCompleted;
+                if (totVaccinesAdministered == null) {
+                    console.log("Total Vaccinations Completed: " + totVaccinesCompleted);
+                }
+                else {
+                    console.log("Total Vaccinations Administered: " + totVaccinesAdministered);
+                }
             // Grab total cases of this state
             let totCases = res.actuals.cases;
             console.log("Total Cases: " + totCases);
@@ -33,14 +39,95 @@ function fetchCovidData(cityAC) {
                 // Empty array = []
                 let arr = [];
                 // Push population to array = [population]
+                //let populationData = 
                 arr.push(totPopulation);
                 // Push vaccinations to array = [population, vaccinations]
-                arr.push(totVaccinesAdministered);
+                //let vaccineData = 
+                if (totVaccinesAdministered ==! null) {
+                    arr.push(totVaccinesAdministered);
+                }
+                else {
+                    arr.push(totVaccinesCompleted);
+                    }
                 // Push cases to array = [population, vaccinations, cases]
-                arr.push(totCases);
+                let casesData = 
+                //arr.push(totCases);
                 // Push deaths to array = [population, vaccinations, cases, deaths]
+                //let deathsData = 
                 arr.push(totDeaths);
                 console.log(arr)
 
         })
 }
+
+/* YouTube video instructions - failed
+const barChart = {
+    chart: null,
+    data: [
+        ["Product", "Sales"],
+        ["Laptops", 1708],
+        ["Desktops", 1457],
+        ["Cameras", 660],
+        ["Phones", 1507],
+        ["Accessories", 768]
+    ],
+    element: "#barchart",
+    options: {
+        title: "2019 Units Sold",
+        width: 500,
+        height: 300
+    }
+};
+
+const init = () => {
+    barChart.chart = new google.visualization.BarChart(
+        document.querySelector(barChart.element)
+    );
+    barChart.chart.draw(
+        google.visualization.arrayToDataTable(barChart.data),
+        barChart.options
+    );
+};
+    google.charts.load("current", {
+        packages:["corechart"],
+        callback: init
+    });
+*/
+
+// Google Charts docs instructions:
+
+// Load the Visualization API and the corechart package.
+ google.charts.load('current', {
+    'packages':
+    ['corechart']
+});
+
+ // Set a callback to run when the Google Visualization API is loaded.
+ google.charts.setOnLoadCallback(drawChart);
+
+ // Callback that creates and populates a data table,
+ // instantiates the pie chart, passes in the data and
+ // draws it.
+ function drawChart() {
+
+   // Create the data table.
+   var data = new google.visualization.DataTable();
+   data.addColumn('string', 'Topping');
+   data.addColumn('number', 'Slices');
+   data.addRows([
+     ['Mushrooms', 3],
+     ['Onions', 1],
+     ['Olives', 1],
+     ['Zucchini', 1],
+     ['Pepperoni', 2]
+   ]);
+
+   // Set chart options
+   var options = {'title':'How Much Pizza I Ate Last Night',
+                  'width':500,
+                  'height':300};
+
+   // Instantiate and draw our chart, passing in some options.
+   var chart = new google.visualization.PieChart(document.getElementById('#bar-chart'));
+   chart.draw(data, options);
+ }
